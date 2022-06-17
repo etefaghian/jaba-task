8acdf53d3931231af4b6926f85d8b3ce08e1e116
etefaghian <etefaghian@yahoo.com>
2022-06-17 18:46:40 +0430

    ♻️ refactor auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import * as jwt from 'jsonwebtoken';
+import { ONE_DAY } from 'utils/date';
 @Injectable()
 export class AuthService {
+  private secret: string;
+  constructor(private configService: ConfigService) {
+    this.secret = configService.getOrThrow('JWT_SECRET');
+  }
+
+  /**
+   * @method
+   * validate access token
+   * @note use another method for refresh token
+   * @param token
+   * @returns
+   */
   verifyJwt(token: string): UserJwtData {
-    const data: UserJwtData = jwt.verify(token, secret) as any;
+    const data: UserJwtData = jwt.verify(token, this.secret) as any;
 
     if (data.typ !== 'access') {
       throw new Error('type of token is not correct ');
@@ -12,23 +26,38 @@ export class AuthService {
     return data;
   }
 
+  /**
+   * @method
+   * generate new jwt token with given data
+   * @remark exp set according to typ field in input data
+   * @param data
+   * @returns
+   */
   constructJwt(data: UserJwtData) {
     //construct jwt token from input
     const token = jwt.sign(
       {
+        ...data,
         exp:
           data.typ === 'access'
-            ? Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7
-            : Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 60,
-        ...data,
+            ? Math.floor(Date.now() / 1000) +
+              ONE_DAY *
+                this.configService.getOrThrow('JWT_ACCESS_TOKEN_TIME_IN_DAYS')
+            : Math.floor(Date.now() / 1000) +
+              ONE_DAY *
+                this.configService.getOrThrow('JWT_REFRESH_TOKEN_TIME_IN_DAYS'),
       },
-      secret,
+      this.secret,
     );
     //return jwt token
     return token;
   }
 }
 
+/**
+ * @interface
+ * represent user data that we store in jwt
+ */
 export interface UserJwtData {
   uid: string;
   ver: number;
@@ -39,5 +68,7 @@ export interface UserJwtData {
   new: boolean;
 }
 
+/**
+ * represent variant type of jwt token
+ */
 export type JwtTokenType = 'access' | 'refresh';
-export const secret = 'secret';
